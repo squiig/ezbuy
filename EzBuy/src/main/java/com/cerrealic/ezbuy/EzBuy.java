6ed2784907d05b8a2a3e07f3c1859747e1dfe22d
Stan G <8138029+squiig@users.noreply.github.com>
2020-04-07 01:21:43 +0200

    Migrated to intellij gradle

---
+++ EzBuy/src/main/java/com/cerrealic/ezbuy/EzBuy.java
@@ -0,0 +1,30 @@
+package com.cerrealic.ezbuy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EzBuy extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		if (!isSpigotServer()) {
+			getLogger().severe("Plugin disabled!");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+
+		this.getCommand("buy").setExecutor(new CommandBuy());
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	private boolean isSpigotServer() {
+		if (getServer().getVersion().contains("Spigot"))
+			return true;
+
+		getLogger().severe("You're probably running a CraftBukkit server. For this to plugin to work you need to switch to Spigot AND use BungeeCord.");
+		return false;
+	}
+}
