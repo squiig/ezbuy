8b6eb1c291739e26dd78439591dc8a7ec549d006
Stan G <8138029+squiig@users.noreply.github.com>
2020-04-07 02:04:49 +0200

    Integrated Vault

--- EzBuy/src/main/java/com/cerrealic/ezbuy/CommandBuy.java
+++ EzBuy/src/main/java/com/cerrealic/ezbuy/CommandBuy.java
@@ -1,5 +1,8 @@
 package com.cerrealic.ezbuy;
 
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,6 +11,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class CommandBuy implements CommandExecutor {
+
+	private EzBuy plugin;
+
+	public CommandBuy(EzBuy plugin) {
+		this.plugin = plugin;
+	}
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -21,7 +30,20 @@ public class CommandBuy implements CommandExecutor {
 		}
 
 		Player player = (Player) sender;
-		int amount = -1;
+		Economy economy = plugin.getEconomy();
+
+		// Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
+		sender.sendMessage(String.format("You have %s",
+				economy.format(economy.getBalance(Bukkit.getOfflinePlayer(player.getUniqueId())))));
+		EconomyResponse r = economy.depositPlayer(player, 1.05);
+		if(r.transactionSuccess()) {
+			sender.sendMessage(String.format("You were given %s and now have %s", economy.format(r.amount), economy.format(r.balance)));
+		} else {
+			sender.sendMessage(String.format("An error occurred: %s", r.errorMessage));
+		}
+
+		/*
+		int amount = 1;
 
 		try {
 			amount = Integer.parseInt(args[1]);
@@ -33,12 +55,13 @@ public class CommandBuy implements CommandExecutor {
 			return false;
 		}
 
-		if (amount < 0) {
+		if (amount <= 0) {
 			return true;
 		}
 
 		ItemStack diamonds = new ItemStack(Material.DIAMOND, amount);
 		player.getInventory().addItem(diamonds);
+		*/
 		return true;
 	}
 
--- EzBuy/src/main/java/com/cerrealic/ezbuy/EzBuy.java
+++ EzBuy/src/main/java/com/cerrealic/ezbuy/EzBuy.java
@@ -1,18 +1,38 @@
 package com.cerrealic.ezbuy;
 
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EzBuy extends JavaPlugin {
 
+	private static Economy economy = null;
+
 	@Override
 	public void onEnable() {
 		if (!isSpigotServer()) {
-			getLogger().severe("Plugin disabled!");
-			getServer().getPluginManager().disablePlugin(this);
+			getLogger()
+					.severe("You're probably running a CraftBukkit server. For this to plugin to "
+							+ "work you need to switch to Spigot AND use BungeeCord.");
+			disablePlugin();
 			return;
 		}
 
-		this.getCommand("buy").setExecutor(new CommandBuy());
+		if (!serverHasVault()) {
+			getLogger().severe("This plugin requires Vault, which you don't seem to have "
+					+ "installed. Please install the Vault plugin.");
+			disablePlugin();
+			return;
+		}
+
+		if (!trySetupEconomy()) {
+			getLogger().severe("Could not detect an economy service! Something probably went "
+					+ "wrong with Vault.");
+			disablePlugin();
+			return;
+		}
+
+		this.getCommand("buy").setExecutor(new CommandBuy(this));
 	}
 
 	@Override
@@ -20,11 +40,32 @@ public class EzBuy extends JavaPlugin {
 
 	}
 
-	private boolean isSpigotServer() {
-		if (getServer().getVersion().contains("Spigot"))
-			return true;
+	private void disablePlugin() {
+		getLogger().severe("Plugin disabled!");
+		getServer().getPluginManager().disablePlugin(this);
+	}
 
-		getLogger().severe("You're probably running a CraftBukkit server. For this to plugin to work you need to switch to Spigot AND use BungeeCord.");
-		return false;
+	private boolean serverHasVault() {
+		return getServer().getPluginManager().getPlugin("Vault") != null;
+	}
+
+	private boolean trySetupEconomy() {
+		RegisteredServiceProvider<Economy> rsp =
+				getServer().getServicesManager().getRegistration(Economy.class);
+
+		if (rsp == null) {
+			return false;
+		}
+
+		economy = rsp.getProvider();
+		return economy != null;
+	}
+
+	private boolean isSpigotServer() {
+		return getServer().getVersion().contains("Spigot");
+	}
+
+	public static Economy getEconomy() {
+		return economy;
 	}
 }
