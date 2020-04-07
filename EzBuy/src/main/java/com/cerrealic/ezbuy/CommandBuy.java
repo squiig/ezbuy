05d146157b9ae62f284d12419a8fd669b41065a9
Stan G <8138029+squiig@users.noreply.github.com>
2020-04-07 02:26:01 +0200

    Trying out withdrawing instead of depositing

--- EzBuy/src/main/java/com/cerrealic/ezbuy/CommandBuy.java
+++ EzBuy/src/main/java/com/cerrealic/ezbuy/CommandBuy.java
@@ -4,6 +4,7 @@ import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,7 +22,7 @@ public class CommandBuy implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (!(sender instanceof Player)) {
-			sender.sendMessage("The /buy command is only available to players.");
+			sender.sendMessage("&cThe /buy command is only available to players.");
 			return true;
 		}
 
@@ -29,29 +30,21 @@ public class CommandBuy implements CommandExecutor {
 			return false;
 		}
 
-		Player player = (Player) sender;
+		OfflinePlayer player = Bukkit.getOfflinePlayer(((Player) sender).getUniqueId());
 		Economy economy = plugin.getEconomy();
 
-		// Lets give the player 1.05 currency (note that SOME economic plugins require rounding!)
-		sender.sendMessage(String.format("You have %s",
-				economy.format(economy.getBalance(Bukkit.getOfflinePlayer(player.getUniqueId())))));
-		EconomyResponse r = economy.depositPlayer(player, 1.05);
-		if(r.transactionSuccess()) {
-			sender.sendMessage(String.format("You were given %s and now have %s", economy.format(r.amount), economy.format(r.balance)));
-		} else {
-			sender.sendMessage(String.format("An error occurred: %s", r.errorMessage));
-		}
+		sender.sendMessage(String.format("You have %s",	economy.format(economy.getBalance(player))));
 
-		/*
 		int amount = 1;
 
 		try {
 			amount = Integer.parseInt(args[1]);
 		} catch (NumberFormatException ex) {
-			sender.sendMessage("Buying failed: The amount you entered was not a valid number.");
+			sender.sendMessage("&cBuying failed: The amount you entered was not a valid number.");
 			return false;
 		} catch (Exception e) {
-			sender.sendMessage("Buying failed: There was an unexpected error with the amount you entered. Please ask a server admin for help.");
+			sender.sendMessage("&cBuying failed: There was an unexpected error with the amount you"
+					+ " entered. Please ask a server admin for help.");
 			return false;
 		}
 
@@ -59,6 +52,15 @@ public class CommandBuy implements CommandExecutor {
 			return true;
 		}
 
+		EconomyResponse r = economy.withdrawPlayer(player, amount);
+
+		if(r.transactionSuccess()) {
+			sender.sendMessage(String.format("You were given %s and now have %s", economy.format(r.amount), economy.format(r.balance)));
+		} else {
+			sender.sendMessage(String.format("&cAn error occurred: %s", r.errorMessage));
+		}
+
+		/*
 		ItemStack diamonds = new ItemStack(Material.DIAMOND, amount);
 		player.getInventory().addItem(diamonds);
 		*/
